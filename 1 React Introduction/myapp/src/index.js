693f4289789c8c884504367995419989d1260a37
gauravKothari <kotharigaurav6@gmail.com>
2025-04-24 11:12:59 +0530

    react js

---
+++ 1 React Introduction/myapp/src/index.js
@@ -0,0 +1,16 @@
+/*
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render("This is an example of React Js",document.getElementById("root"));
+*/
+
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+/*
+const root = document.getElementById("root");
+const myRoot = createRoot(root);
+myRoot.render("This is an example of React Js");
+*/
+
+createRoot(document.getElementById("root")).render("This is an example of React Js..!!!");
